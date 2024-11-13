8c6adf4506fc79c3744f6d82a25fce4bcac40e16
Daniel Markusson <46487826+Dmarky9873@users.noreply.github.com>
2024-11-12 23:45:09 -0500

    gitignore update

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from django.middleware.csrf import get_token
-from django.shortcuts import render
+# from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import MessageSerializer
 from .models import Message
