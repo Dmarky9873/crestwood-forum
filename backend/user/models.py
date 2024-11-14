7b928017792a976ad6f8ae09bd37508cfe7c495e
Daniel Markusson <46487826+Dmarky9873@users.noreply.github.com>
2024-11-14 10:38:10 -0500

    made serializer for users

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
-    uuid = models.UUIDField(primary_key = False)
-    username = models.CharField(max_length=255, blank = True, null = True, unique = True, verbose_name=("Username"))
+    uuid = models.UUIDField(primary_key=False)
+    username = models.CharField(
+        max_length=255, blank=True, null=True, unique=True, verbose_name=("Username"))
     password = models.CharField(max_length=255)
-    email = models.EmailField(unique = True, verbose_name=('Email Address'))
+    email = models.EmailField(unique=True, verbose_name=('Email Address'))
     firstName = models.CharField(max_length=255)
     lastName = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.username
+        return str(self.username)
