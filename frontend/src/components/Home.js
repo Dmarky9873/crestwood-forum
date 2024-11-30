0c579a7bdf7fa859ad004e21b7bd494b54a87ad0
Daniel Markusson <46487826+Dmarky9873@users.noreply.github.com>
2024-11-30 12:21:15 -0500

    got a welcome page with the user's username

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { Component } from "react";
 import { Navigate, redirect } from 'react-router-dom';
 
@@ -6,6 +7,7 @@ class Home extends Component {
     state = {
         redirectToSignup: false,
         redirectToMessaging: false,
+        username: "",
     };
 
     redirectSignup = () => {
@@ -20,6 +22,20 @@ class Home extends Component {
         this.setState({ redirectLogin: true });
     }
 
+    getUsername = () => {
+        axios.get("/accounts/api/username/")
+            .then((response) => {
+                this.setState({ username: response.data.username }); // Update state
+            })
+            .catch((error) => {
+                console.error("Error fetching username:", error);
+            });
+    }
+
+    componentDidMount() {
+        this.getUsername();
+    }
+
     render() {
         if (this.state.redirectToSignup) {
             return <Navigate to="/signup" />;
@@ -31,6 +47,7 @@ class Home extends Component {
         return (
             <main className="container">
                 <h1 className="text-uppercase text-center my-4">Crestwood-Forum Home</h1>
+                <h2 className="text-uppercase text-center my-4">Welcome, {this.state.username}</h2>
                 <button
                     className="btn btn-primary"
                     onClick={this.nav}
